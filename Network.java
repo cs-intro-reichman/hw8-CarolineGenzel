28828e7d0bb0c7071744192e4703cb063fab3a88
Caroline Genzel <carolinegenzel@MacBook-Pro-sl-Caroline.local>
2025-01-07 18:42:17 +0200

    finished task

--- Network.java
+++ Network.java
@@ -125,13 +125,22 @@ public class Network {
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
-        //// Replace the following statement with your code
-        return 0;
+        if (getUser(name)  == null) {
+            return 0;
+        }
+        return 1;
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       //// Replace the following statement with your code
-       return null;
+       String all=null;
+        for(int i = 0; i < this.users.length; i++){
+        String ans = this.users[i].getName() + " -> ";
+        for (int j = 0; j < this.users[i].getfCount(); j++) {
+            ans = ans + this.users[i].getfFollows()[j] + " ";
+        }
+        all += ans;
+       }
+      return all;
     }
 }
